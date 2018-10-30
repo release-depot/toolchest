559d68970f436b1b973627ca7cb42aa5d197cb9f
Jason Joyce <fuzzball81@gmail.com>
2018-10-30 08:40:51 -0400

    Rotating the packaging to be a little more flexible.
    Updating the packaging to better allow for adding dependencies and
    managing everything through setup.py. Now the requirements files use the
    data from setup.py directly without having to replicate data in two
    places.
    
    Updated tox to use the new layout correctly.
    
    Dropping support for python 3.4 and 3.5 since 3.6 is more common. It
    also allows for testing with newer versions of pytest and PyYAML. Adding
    python 3.7 testing support.
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # Copyright (c) 2018, Red Hat, Inc.
 #   License: MIT
-import setuptools
-import re
-
-from toolchest import __version__
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -13,22 +10,18 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 
-def requires(prefix=''):
-    """Retrieve requirements from requirements.txt
-    """
-    try:
-        reqs = map(str.strip, open(prefix + 'requirements.txt').readlines())
-        reqs = filter(lambda s: re.match(r'\W', s), reqs)
-        return reqs
-    except Exception:
-        pass
-    return []
+TEST_REQUIRES = ['coverage', 'flake8', 'pytest', 'pytest-datadir', 'tox']
 
-
-setuptools.setup(
+setup(
     name='toolchest',
-    version=__version__,
-    install_requires=requires(),
+    version='0.0.2',
+    setup_requires=['pytest-runner'],
+    install_requires=[],
+    tests_require=TEST_REQUIRES,
+    extras_require={'test': TEST_REQUIRES,
+                    'docs': ['sphinx',
+                             'sphinx-autobuild',
+                             'sphinx-rtd-theme']},
     license='MIT',
     description=("toolchest is a collection of generic "
                  "functions for release-depot."),
@@ -37,17 +30,18 @@ setuptools.setup(
     author_email='lhh@redhat.com',
     maintainer='Lon Hohberger',
     maintainer_email='lon@metamorphism.com',
-    packages=['toolchest', 'toolchest.rpm'],
+    packages=find_packages(include=['toolchest.*']),
     url='http://github.com/release-depot/toolchest',
     data_files=[("", ["LICENSE"])],
+    test_suite='tests',
+    zip_safe=False,
     classifiers=['Development Status :: 4 - Beta',
                  'Intended Audience :: Developers',
                  'Natural Language :: English',
                  'Operating System :: POSIX :: Linux',
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  'Topic :: Software Development',
                  'Topic :: Software Development :: Libraries',
                  'Topic :: Software Development :: Libraries :: Python Modules',
