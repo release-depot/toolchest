6541ad8b0d3b22b4301bb4adf308833d3f42c13e
Jason Joyce <fuzzball81@gmail.com>
2020-11-18 08:33:53 -0500

    Adding a write method to the yaml module.
    Adding the ability to write a yaml file.
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

--- toolchest/yaml.py
+++ toolchest/yaml.py
@@ -4,6 +4,16 @@
 import yaml
 
 
+class IndentDumper(yaml.Dumper):
+    """ Custom yaml dumper to ensure elements are indented """
+    def increase_indent(self, flow=False, indentless=False):
+        # We can't set indentless from the yaml.dump call to control behavior.
+        # When this method is called, indentless is being set to True so it
+        # can't be used in the call to super. We the super call to always be
+        # False.
+        return super().increase_indent(flow=flow, indentless=False)
+
+
 def parse(yaml_data, logger=None):
     """
     Safely parse yaml data
@@ -26,3 +36,22 @@ def parse(yaml_data, logger=None):
         else:
             print(msg)
     return ret_data
+
+
+def write(filename, yaml_data, sort_keys=False):
+    """
+    Write out yaml data to a file
+
+    :param filename: The name of the file to write the data
+    :param yaml_data: A string of yaml data to be parsed
+    :param sort_keys: Have the yaml data's keys sorted on write
+    :return: None
+    """
+
+    # open the file for write, if it does not exist create it.
+    with open(filename, 'w') as output_file:
+        # write yaml header
+        output_file.write('---\n\n')
+        # write the actual data
+        yaml.dump(yaml_data, output_file, Dumper=IndentDumper,
+                  default_flow_style=False, sort_keys=sort_keys)
