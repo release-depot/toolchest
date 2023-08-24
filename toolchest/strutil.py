32cf97f82ef124412b530f8a875f2e0b5f7bda3e
Lon Hohberger <lhh@redhat.com>
2023-08-24 14:35:31 -0400

    strutil: Add 'nym' function
    Helper function for dealing with names while avoiding pesky
    things like quoting, spaces, and case sensitivity

--- toolchest/strutil.py
+++ toolchest/strutil.py
@@ -30,3 +30,24 @@ def regex_match(pattern, arg):
     if re.match(pattern, arg) is not None:
         return True
     return False
+
+
+def nym(arg):
+    '''
+    This is for allowing case and quote flexibility for strings when
+    searching dictionaries or other data sets based on user input (esp.
+    from the command line) where the likelihood of key collisions is
+    low. For example, if we want to search a dictionary, we'd check the
+    nym of the value provided with the nym of the key to see if they
+    match. This should not be used when likelihood of collisions is high.
+    (Origin: Greek word meaning "name")
+
+    Parameters:
+        arg (string): A string to create the nym for
+
+    Returns:
+        ret (string): A lower-case string with whitespace swapped to _
+    '''
+    ret = arg.lower().replace(' ', '_')
+    ret = ret.replace('\t', '_')
+    return ret
